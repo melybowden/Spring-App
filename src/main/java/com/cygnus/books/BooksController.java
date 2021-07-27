1a2a94032683042b615228aa746ed014375df777
Madison Bowden <melybowden@gmail.com>
2021-07-27 14:16:41 -0400

    rm webconfig

--- src/main/java/com/cygnus/books/BooksController.java
+++ src/main/java/com/cygnus/books/BooksController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.web.servlet.headers.HeadersSecurityMarker;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;

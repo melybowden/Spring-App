2e53336fac43027b4835f3ac20de368f9d6a5260
Madison Bowden <melybowden@gmail.com>
2021-07-27 14:22:59 -0400

    permit all

--- src/main/java/com/cygnus/books/WebConfiguration.java
+++ src/main/java/com/cygnus/books/WebConfiguration.java
@@ -1,44 +1,45 @@
-// package com.cygnus.books;
+package com.cygnus.books;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-// @Configuration
-// @EnableWebSecurity
-// public class WebConfiguration extends WebSecurityConfigurerAdapter {
-//     @Autowired
-//     BCryptPasswordEncoder encoder;
+@Configuration
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    BCryptPasswordEncoder encoder;
 
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http
-//             .httpBasic()
-//             .and()
-//                 .authorizeRequests()
-//                 .antMatchers(HttpMethod.GET, "/book").permitAll() // Everyone can READ and LIST from /book
-//                 .antMatchers(HttpMethod.POST, "/book").permitAll()
-//                 .anyRequest().authenticated() // All other requests must be authenticated
-//             .and()
-//                 .csrf().disable(); // Unsafe for PROD
-//     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic()
+            .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                // .antMatchers(HttpMethod.GET, "/book").permitAll() // Everyone can READ and LIST from /book
+                // .antMatchers(HttpMethod.POST, "/book").permitAll()
+                // .anyRequest().authenticated() // All other requests must be authenticated
+            .and()
+                .csrf().disable(); // Unsafe for PROD
+    }
 
-//     @Override 
-//     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//         auth.inMemoryAuthentication()
-//             .withUser("testuser")
-//             .password(encoder.encode("testpassword"))
-//             .roles("USER");
-//     }
+    @Override 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("testuser")
+            .password(encoder.encode("testpassword"))
+            .roles("USER");
+    }
 
-//     @Bean
-//     public BCryptPasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

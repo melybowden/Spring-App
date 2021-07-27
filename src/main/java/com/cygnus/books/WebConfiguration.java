e8deaf61eaa1765aa87af5f264075248e4c29a0d
Madison Bowden <melybowden@gmail.com>
2021-07-27 10:26:41 -0400

    add spring security and setup heroku properties

---
+++ src/main/java/com/cygnus/books/WebConfiguration.java
@@ -0,0 +1,43 @@
+package com.cygnus.books;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    BCryptPasswordEncoder encoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic()
+            .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/book").permitAll() // Everyone can READ and LIST from /book
+                .anyRequest().authenticated() // All other requests must be authenticated
+            .and()
+                .csrf().disable(); // Unsafe for PROD
+    }
+
+    @Override 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("testuser")
+            .password(encoder.encode("testpassword"))
+            .roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
